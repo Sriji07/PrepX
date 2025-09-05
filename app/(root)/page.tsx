fb61152c72f75b884e357f6ac8e7784d09775090
Srijita <srijitakook@gmail.com>
2025-09-05 22:17:43 +0530

    First commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from '@/components/ui/button';
+const page = () => {
+  return (
+    <div>page
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default page
